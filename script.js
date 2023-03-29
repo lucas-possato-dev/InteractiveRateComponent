3554e1ba3246162ec7157e2bdb0a4d868d6fc3aa
lucas-possato-dev <lucaspstinho@gmail.com>
2023-03-29 11:31:59 -0300

    errorUpdate

--- script.js
+++ script.js
@@ -4,33 +4,43 @@ const rate = document.querySelectorAll(".rate button");
 const btn = document.querySelector(".btn");
 const ids = document.querySelectorAll(".rateNumber");
 const spanContainer = document.querySelector(".spanContainer span")
-console.log(ids)
 
 
 // Submit 
 
 
 function handleSend() {
-  howDid.classList.add("change");
-  thankYou.classList.add("change");
-}
+  const selectedButton = document.querySelector(".rate button.selected")
+  if(selectedButton) {
+    howDid.classList.add("change");
+    thankYou.classList.add("change");
+    }
+  else {
+    rate.forEach((button) => {
+      button.classList.add("error");
+    })
+  }
+  }
+  
 
 btn.addEventListener("click", handleSend);
 
 
 // Selecionando buttons
 
-function rateSelect(event, index) {
+function rateSelect(event) {
   rate.forEach((button) => {
     button.classList.remove("selected");
+    button.classList.remove("error");
   });  
   const selectedButton = event.currentTarget;
   selectedButton.classList.toggle("selected");
-  if(handleSend) {
-    spanContainer.innerText = `You selected ${selectedButton.innerText} out of 5`
-  }
+  
+   
+    if(handleSend) {
+      spanContainer.innerText = `You selected ${selectedButton.innerText} out of 5`
+    }
 }
-
 
 rate.forEach((button) => {
   button.addEventListener("click", rateSelect )
